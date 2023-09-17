2f97238b2d862e18ebef603ca09e4585762f9ce8
Siyuan <momme.solga@web.de>
2023-09-17 14:31:40 +0200

    bugfixing

--- zeichenrobo_übertragung.py
+++ zeichenrobo_übertragung.py
@@ -4,13 +4,10 @@ import time
 
 arduino = serial.Serial(port = 'COM3', baudrate =115200, timeout = .1)
 
-coordinates = [1,2,3,4,5,6,7,8,9,10]
+coordinates = [190, 300, 400,200, 500, 200, 190, 290 ]
 
 def write_read(x):
     arduino.write(bytes(str(x), 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
 
 for y in range(0, len(coordinates)//2):
     a = coordinates[2 * y]

a54569e9fa5fd35ea7523c37813c2679d35aa544
Siyuan <momme.solga@web.de>
2023-09-17 14:53:35 +0200

    demo

--- sketch_sep17a/sketch_sep17a.ino
+++ sketch_sep17a/sketch_sep17a.ino
@@ -1,12 +1,32 @@
-int x;
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(1);
+  pwm.begin();
+  pwm.setPWMFreq(50);  // Set the PWM frequency (usually 60 Hz)
 }
 
 void loop() {
-  while (!Serial.available());
-  x = Serial.readString().toInt();
-  Serial.print(x);
+
+  /*if(Serial.available()){
+    int x = Serial.readString().toInt();
+    int y = Serial.readString().toInt();
+    //      Servo,   PWM
+    pwm.setPWM(0, 0, x);
+    pwm.setPWM(1, 0, y);
+  }*/
+
+  for(int i = 0; i < 10; i++) {
+    pwm.setPWM(1, 0, 400+i*15);
+    for(int j = 0; j < 50; j++) {
+      pwm.setPWM(0, 0, 200+j*3);
+      delay(100);
+    }
+
+  }
 }
+
